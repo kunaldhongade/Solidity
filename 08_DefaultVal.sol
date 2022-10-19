cf131fd237b7c5174c80ad30b7dfbc7452851a01
Kunal Dhongade <74715881+kunaldhongade@users.noreply.github.com>
2022-10-19 21:27:15 +0530

    Adding Default Varible in sol to repo

---
+++ 08_DefaultVal.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract DefaultVal {
+    uint public x;
+    address public y;
+    bool public z;
+    bytes32 public a;
+    string public str;
+}
